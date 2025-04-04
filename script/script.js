d35fa62c23af436f45f3a51130c2c5cf5e59728e
neocari <melgarralph@gmail.com>
2025-04-04 16:19:25 -0600

    Added other required java elements for lab

--- script/script.js
+++ script/script.js
@@ -2,10 +2,8 @@
 function showMessage()
 {
   
-    let x = 5;
-    let name = "Fire Giant";
-    let msg = "Hello "  +name + " whats up";
-    let reply = "AAAGGHHHHHHHH"
+    let msg = "Hello World!";
+    let reply = "Ahh!"
     alert(msg);
     alert(reply)
 }
@@ -35,18 +33,25 @@ function changeColor()
 {
     document.getElementById("main").style.backgroundColor="#fff";
     document.getElementById("main").style.color="black" ;
-    if (backgroundColor="fff")
-    {
-        backgroundColor=""
-    }
+
 }
 
+function changeColorBack()
+{
+    document.getElementById("main").style.backgroundColor="#701b1b";
+    document.getElementById("main").style.color="white" ;
+}
 function GoTo()
 {
     window.open("https://www.solomusicgear.com")
 }
 
-function changeTheme()
-{
-    document.getElementById("css").style.href="./css/stylesalternate.css"
-}
+function countUp()
+{let count = 0;
+    document.getElementById('buttonCounter').onclick = function() {
+         count = count + 1;
+         document.getElementById('counter').innerHTML = "You have clicked the counter " + count + " times."
+    }}
+
+
+
